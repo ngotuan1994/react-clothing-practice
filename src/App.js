f9415faa838b7fd7d9d0fa62d42c8dac0ea89fd2
Tuan Ngo <ngotuan911@gmail.com>
2022-01-04 23:00:37 -0800

    Added footer components

--- src/App.js
+++ src/App.js
@@ -4,14 +4,19 @@ import { Routes, Route } from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import Footer from './components/footer/footer.component';
+// import Contact from './components/contact/contact.component'
+
 function App() {
   return (
     <div>
         <Header></Header>
         <Routes>
           <Route  path="/" element={<Homepage/>} ></Route>
-          <Route   path='/shop' element={<ShopPage/>}></Route>
+          <Route path='/shop' element={<ShopPage />}></Route>
+          {/* <Route path='/contact' element={<Contact />}></Route>  */}
         </Routes>
+        <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/footer/footer.component.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import {Link} from 'react-router-dom'
+import { ReactComponent as Logo } from "../../assets/crown.svg"
+
+import './footer.styles.scss'
+
+
+const Footer = () => {
+  return (
+    <footer className="footer-distributed">
+
+			<div className="footer-left">
+        <div>
+          <Logo classname="logo"></Logo>
+
+        </div>
+
+
+				<Link to ="/shop" className="footer-links">
+					<Link to="/">Honme.</Link>
+
+					<Link to="/">Blog.</Link>
+
+					<Link to="/">Pricing.</Link>
+
+					<Link to="/">About.</Link>
+
+					<Link to="/">Faq.</Link>
+
+					<Link to="/">Contact.</Link>
+				</Link>
+
+				<p className="footer-company-name">Company Name © 2022</p>
+
+				<div className="footer-icons">
+
+					<Link to="/"><i className="fa-brands fa-facebook"></i></Link>
+					<Link to="/"><i className="fa-brands fa-twitter"></i></Link>
+					<Link to="/"><i className="fa-brands fa-linkedin"></i></Link>
+					<Link to="/"><i className="fa-brands fa-github"></i></Link>
+
+				</div>
+
+			</div>
+
+			<div className="footer-right">
+
+				<p>Contact Us</p>
+
+				<form action="#" method="post">
+
+					<input type="text" name="email" placeholder="Email"></input>
+					<textarea name="message" placeholder="Message"></textarea>
+					<button>Send</button>
+
+				</form>
+
+			</div>
+
+		</footer>
+  );
+}
+
+export default Footer;
