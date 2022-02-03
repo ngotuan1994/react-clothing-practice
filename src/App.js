fc016f5e3b3d53b8a4360e66642467a8251050fe
Tuan Ngo <ngotuan911@gmail.com>
2022-02-02 17:26:18 -0800

    Change App class component to functional component

--- src/App.js
+++ src/App.js
@@ -8,46 +8,42 @@ import Footer from './components/footer/footer.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { auth } from "./firebase/firebase.utils"
 import { createUserPorfileDocument } from './firebase/firebase.utils';
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      currentUser: null
-    };
-  }
-  unsubcribeFromAuth = null;
-  componentDidMount() {
-    this.unsubcribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+import { useState, useEffect } from 'react';
+function App () {
+  const [currentUser, setcurrentUser] = useState(null);
+  useEffect(() => {
+    const unsubcribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
-          const userRef = await createUserPorfileDocument(userAuth);
-          userRef.onSnapshot(snapShot => {
-            this.setState({
-              currentUser: {
-                id: snapShot.id,
-                ...snapShot.data()
-              }
-            }, () => {
-              console.log(this.state.currentUser);
-            })
+        const userRef = await createUserPorfileDocument(userAuth);
+        userRef.onSnapshot(snapShot => {
+          setcurrentUser({
+
+              id: snapShot.id,
+              ...snapShot.data()
+
+          }, () => {
+            console.log(currentUser);
           })
-        }
+        })
+      }
       else {
-          this.setState({ currentUser: userAuth });
+        this.setState({ currentUser: userAuth });
       }
 
     });
-  }
-
-  componentWillUnmount() {
-    this.unsubcribeFromAuth();
-  }
-
+    return () => {
+      unsubcribeFromAuth();
+    }
+  }, []);
 
 
-  render() {
+
+
+
+
     return (
       <div>
-        <Header currentUser={this.state.currentUser} ></Header>
+        <Header currentUser={currentUser} ></Header>
         <Routes>
           <Route path="/" element={<Homepage />} ></Route>
           <Route path='/shop' element={<ShopPage />}></Route>
@@ -58,6 +54,6 @@ class App extends React.Component {
     );
 
   };
-}
+
 
 export default App;
